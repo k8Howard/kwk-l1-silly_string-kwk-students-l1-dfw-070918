e07cceb0efe5323865f6dd3c9e6bc1dc7102eb33
k8Howard <sweetk8@me.com>
2018-07-09 20:16:17 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -33,11 +33,11 @@ bigger_word = big_word + "wow" + "mom" # Use String concatenation to add "wow" a
 
 new_letter_count = bigger_word.size
 
-puts "There are now #{new_letter_count} in #{bigger_word}"
+puts "There are now #{new_letter_count} letters in #{bigger_word}"
 
 # Challenge 6: Capitalize this sentence properly
 
-proper_sentence = "i really like programming." # Put a . after the string to capitalize it properly.
+proper_sentence = "i really like programming.".capitalize # Put a . after the string to capitalize it properly.
 
 puts proper_sentence
 
